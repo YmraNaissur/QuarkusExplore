983704c5ab745324f3445f34785a39e611f86f5e
Maxim Karavaev <maxim.karavaev@t-systems.com>
2020-06-19 19:47:38 +0300

    Create greetin service and inject it into greeting resource

--- src/main/java/tech/donau/course/GreetingResource.java
+++ src/main/java/tech/donau/course/GreetingResource.java
@@ -1,35 +1,22 @@
 package tech.donau.course;
 
-import javax.ws.rs.*;
+import tech.donau.course.service.GreetingService;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import java.util.UUID;
 
 @Path("/hello")
 public class GreetingResource {
-    public static final String TOKEN = "dev";
+    @Inject
+    GreetingService greetingService;
 
     @GET
-    @Path("/html")
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello " + UUID.randomUUID().toString();
-    }
-
-    @GET
-    @Path("/html/{name}")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello(@PathParam("name") String name) {
-        return "Hello, " + name;
-    }
-
-    @POST
-    @Produces(MediaType.TEXT_HTML)
-    @Consumes(MediaType.APPLICATION_JSON)
-    public String addHello(@HeaderParam("token") String hName, @QueryParam("token") String qName, String body) {
-        String token = hName != null ? hName : qName;
-        if (!TOKEN.equals(token)) {
-            throw new RuntimeException("Unauthorized");
-        }
-        return "<h2>Hello, " + token + "!</h2>";
+    public String hello(@QueryParam("name") String name) {
+        return greetingService.sayHello(name);
     }
 }
