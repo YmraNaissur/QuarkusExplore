cc7cbd7522802bec962da8541e731190b0c4e9c6
YmraNaissur <daily.spb@yandex.ru>
2020-06-21 22:16:08 +0300

    Add custom config converter (base64)

--- src/main/java/tech/donau/course/service/GreetingService.java
+++ src/main/java/tech/donau/course/service/GreetingService.java
@@ -1,12 +1,11 @@
 package tech.donau.course.service;
 
-import org.eclipse.microprofile.config.ConfigProvider;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import tech.donau.course.config.Base64Value;
 import tech.donau.course.config.GreetingConfig;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import java.util.Optional;
 import java.util.UUID;
 
 @ApplicationScoped
@@ -15,13 +14,16 @@ public class GreetingService {
     @Inject
     GreetingConfig greetingConfig;
 
+    @ConfigProperty(name = "greeting.base64greet")
+    Base64Value base64greet;
+
     public String sayHello(String name) {
         return String.format("Hello %s, your ID is %s.", name, UUID.randomUUID().toString());
     }
 
     public String sayHello() {
         return greetingConfig.getPrefix().orElse("_")
-                + greetingConfig.getGreet()
+                + base64greet
                 + greetingConfig.getSuffix()
                 + "Your country is " + greetingConfig.getCountry().getName()
                 + "(" + greetingConfig.getCountry().getId() + ").";
